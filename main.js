b793cc81891360b80cdae69a28af517a987d7c83
qq860831 <qq860831@gmail.com>
2020-08-08 17:31:53 +0800

    123

--- main.js
+++ main.js
@@ -8,3 +8,10 @@ $(window).scroll(function(e){
     else
       $(".navbar,.explore").removeClass("at_top");
   });
+//緩慢滑動
+$(document).on('click', 'a', function(event){
+    event.preventDefault();
+    $('html, body').animate({
+        scrollTop: $( $.attr(this, 'href') ).offset().top
+    }, 500);
+});
