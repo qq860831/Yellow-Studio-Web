f7c3fd1f9b47a03344d0b994e9a53f6456957322
Ian <qq860831@gmail.com>
2020-11-06 22:42:52 +0800

    123

--- main.js
+++ main.js
@@ -1,17 +1,29 @@
 $(document).ready(function(){
-    });
-
-//滑動離開頂部時就取消at_top的class
-$(window).scroll(function(e){
-    if ($(window).scrollTop()<=0)
-      $(".navbar,.explore").addClass("at_top");
-    else
-      $(".navbar,.explore").removeClass("at_top");
+  //滑動離開頂部時就取消at_top的class
+  $(window).scroll(function(e){
+    e.preventDefault();
+    if($(window).scrollTop()<=0){
+      $(".explore,.navbar").addClass("at_top");
+    }else{
+      $(".explore,.navbar").removeClass("at_top");
+    }
   });
-//緩慢滑動
-$(document).on('click', 'a', function(event){
-    event.preventDefault();
-    $('html, body').animate({
-        scrollTop: $( $.attr(this, 'href') ).offset().top
-    }, 500);
 });
+
+
+
+
+
+
+
+
+
+
+
+//緩慢滑動
+// $(document).on('click', 'a', function(event){
+//     event.preventDefault();
+//     $('html, body').animate({
+//         scrollTop: $( $.attr(this, 'href') ).offset().top
+//     }, 500);
+// });
