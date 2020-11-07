e98c3eb68bd23571b54e30c66342b2b936e0c4d0
Ian <qq860831@gmail.com>
2020-11-07 21:00:45 +0800

    123

--- main.js
+++ main.js
@@ -7,9 +7,22 @@ $(document).ready(function(){
     }else{
       $(".explore,.navbar").removeClass("at_top");
     }
+
+    
   });
 
-  //平滑滑動到特定段落
+  //滑鼠移動時觸發的事件
+  $(window).mousemove(function(evt){
+  var pagex = evt.pageX;
+  var pagey = evt.pageY;
+
+  //更新一些移動景物的位置
+  $(".mountain").css("transform","translateX("+(x/-20+50)+"px)")
+});
+
+
+
+ // 平滑滑動到特定段落(效果無法顯示)
   // $(document).on('click', 'a', function(e) {
   //     e.preventDefault();
   //     var target = $(this).attr("href");
@@ -19,23 +32,18 @@ $(document).ready(function(){
   // });
 
   //vue監看物件
-  var vm = new Vue({
-    el: "#app",
-    data: {
-      works: []
-    },
-    mounted: function(){
-      var vobj=this;
-      $.ajax({
-        url: "https://awiclass.monoame.com/api/command.php?type=get&name=projects",
-        success: function(res){
-          vobj.works=JSON.parse(res);
-        }
-      });
-    }
-    
-  });
-
-});
-
-
+  // var vm = new Vue({
+  //   el: "#app",
+  //   data: {
+  //     works: []
+  //   },
+  //   mounted: function(){
+  //     var vobj=this;
+  //     $.ajax({
+  //       url: "https://awiclass.monoame.com/api/command.php?type=get&name=projects",
+  //       success: function(res){
+  //         vobj.works=JSON.parse(res);
+  //       }
+  //     });
+  //   }  
+  // });
