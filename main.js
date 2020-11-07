b8bb86ba68fc36448b7f143ecd9c404fcfd2e43c
Ian <qq860831@gmail.com>
2020-11-07 11:16:30 +0800

    123

--- main.js
+++ main.js
@@ -8,22 +8,34 @@ $(document).ready(function(){
       $(".explore,.navbar").removeClass("at_top");
     }
   });
+
+  //平滑滑動到特定段落
+  // $(document).on('click', 'a', function(e) {
+  //     e.preventDefault();
+  //     var target = $(this).attr("href");
+  //     $('html, body').animate({
+  //         scrollTop: $(target).offset().top
+  //     }, 500);
+  // });
+
+  //vue監看物件
+  var vm = new Vue({
+    el: "#app",
+    data: {
+      works: []
+    },
+    mounted: function(){
+      var vobj=this;
+      $.ajax({
+        url: "https://awiclass.monoame.com/api/command.php?type=get&name=projects",
+        success: function(res){
+          vobj.works=JSON.parse(res);
+        }
+      });
+    }
+    
+  });
+
 });
 
 
-
-
-
-
-
-
-
-
-
-//緩慢滑動
-// $(document).on('click', 'a', function(event){
-//     event.preventDefault();
-//     $('html, body').animate({
-//         scrollTop: $( $.attr(this, 'href') ).offset().top
-//     }, 500);
-// });
